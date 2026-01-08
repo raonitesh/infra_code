df3dc5ed9856d812a69c32ef22e491b393982141
Nitesh Rao <niteshrao40m@gmail.com>
2026-01-08 06:59:07 +0530

    new cod32

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.90.0"
+      source = "hashicorp/azurerm"
+      version = "4.57.0"
     }
   }
 
@@ -10,12 +10,12 @@ terraform {
     resource_group_name   = "ash-rg"
     storage_account_name  = "Niteshstg11"
     container_name        = "nitsco"
-    key                   = "nni.terraform.tfstate"
+    key                   = "nniterraform.tfstate"
 
   }
 }
 
 provider "azurerm" {
-  features {}
-  subscription_id = "6f8da1c9-b993-4f71-b180-2c6245a0833d"
+    features {}
+    subscription_id = "6f8da1c9-b993-4f71-b180-2c6245a0833d"
 }
